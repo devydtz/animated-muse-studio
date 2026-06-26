15625c6d103052682ac757f79e47c842bdcbabb7
devydtz <ytvxmfqz66@privaterelay.appleid.com>
2026-06-26 10:50:34 +0800

    Update SiteFooter.tsx

--- src/components/SiteFooter.tsx
+++ src/components/SiteFooter.tsx
@@ -1,5 +1,5 @@
 import { Link } from "@tanstack/react-router";
-import logoAsset from "@/assets/lunaris-logo.png.asset.json";
+import logoAsset from "@/assets/lunaris.logo.png";
 
 export function SiteFooter() {
   return (
